6e89b695412850e97f4411e0598c565b61f13a45
kevAs <kevassiobo@gmail.com>
2021-04-25 12:00:21 +0200

    base

---
+++ main.js
@@ -0,0 +1,27 @@
+let x = prompt('jours de la semaine');
+switch (x) {
+    case "lundi":
+        console.log(' On est lundi. Tu as cours à MolenGeek et tu as sport en soirée.');
+        break;
+    case "mardi":
+        console.log(' On est mardi. Tu as cours à MolenGeek et tu as sport en soirée.');
+        break;
+    case "mercredi":
+        console.log(' On est mercredi. Tu as cours à MolenGeek et tu as sport en soirée.');
+        break;
+    case "jeudi":
+        console.log(' On est jeudi. Tu as cours à MolenGeek et tu as sport en soirée.');
+        break;
+    case "vendredi":
+        console.log(' On est vendredi. Tu as cours à MolenGeek et tu as sport en soirée.');
+        break;
+    case "samedi":
+        console.log(' On est samedi. Tu as cours à MolenGeek et tu as sport en soirée.');
+        break;
+    case "dimanche":
+        console.log(' On est dimache. Tu as cours à MolenGeek et tu as sport en soirée.');
+        break;
+    default:
+        console.log("c'est la fin de la semain");
+        break;
+}
